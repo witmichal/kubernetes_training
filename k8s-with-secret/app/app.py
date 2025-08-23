f7744512043e64d1a2f090c5cc66da1d97f2a8a5
Michal Wit <michal.wit@paramount.com>
2025-08-23 15:41:32 +0200

    duplicate app directory in every directory

---
+++ k8s-with-secret/app/app.py
@@ -0,0 +1,66 @@
+from typing import Optional
+from flask import Flask
+import psycopg2
+import os
+from pathlib import Path
+
+app = Flask(__name__)
+
+
+def read_env_var(key: str) -> Optional[str]:
+	try:
+		return os.environ[key]
+	except KeyError as e:
+		print(repr(e) + f" - no env var defined: {key}")
+		return None
+
+
+'''
+export DB_HOST=localhost
+export DB_NAME=postgres
+export DB_USER=postgres
+export DB_PASS=mypassword
+
+docker run -p 5432:5432 --name postgres-db -e POSTGRES_PASSWORD=mypassword postgres
+psql -h localhost -U postgres -d postgres
+'''
+def build_connection_string() -> str:
+	host = read_env_var('DB_HOST')
+	name = read_env_var('DB_NAME')
+	user = read_env_var('DB_USER')
+	password = read_env_var('DB_PASS')
+
+	creds_mount = "/etc/creds"
+	if password is None:
+		password = Path(f"{creds_mount}/db-password").read_text().strip()
+	if user is None:
+		user = Path(f"{creds_mount}/db-username").read_text().strip()
+
+	return f"dbname='{name}' user='{user}' host='{host}' password='{password}'"
+
+
+def fetch_postgres_version() -> Optional[str]:
+	try:
+		#"dbname='postgres' user='postgres' host='localhost' password='mypassword'"
+		connection_string = build_connection_string()
+		print(f"Connecting to: [{connection_string}]")
+		conn = psycopg2.connect(connection_string)
+		with conn.cursor() as curs:
+			try:
+				curs.execute("SELECT version()")
+				single_row = curs.fetchone()
+				return str(single_row)
+			except (Exception, psycopg2.DatabaseError) as error:
+				print(error)
+	except Exception as e:
+		print(repr(e))
+		print("I am unable to connect to the database")
+
+
+@app.route('/')
+def hello():
+	pg_version = fetch_postgres_version()
+	return f"Hello World! - PG version: {pg_version}"
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
---
+++ k8s-with-volume-secret/app/export_vars.sh
@@ -0,0 +1,4 @@
+export DB_HOST=localhost
+export DB_NAME=postgres
+export DB_USER=postgres
+export DB_PASS=mypassword
